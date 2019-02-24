af28496a4c27018693882dfbd298f20a60f70757
Nagarjun M S <nagarjun.ms@nutanix.com>
2019-02-24 19:52:06 +0530

    Added APIs for User

--- src/api.go
+++ src/api.go
@@ -1,5 +1,26 @@
 package main
 
-func getUsers() {
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
+)
+
+// GetUsers : Get users from database
+func GetUsers(w http.ResponseWriter, request *http.Request) {
+	users := getUsers()
+	json.NewEncoder(w).Encode(users)
+}
+
+// GetUser : Get user from database
+func GetUser(w http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	id, err := strconv.ParseInt(vars["id"], 10, 32)
+	if err != nil {
+		panic(err)
+	}
+	users := getUser(int32(id))
+	json.NewEncoder(w).Encode(users)
 }
--- src/main.go
+++ src/main.go
@@ -1,33 +1,15 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 func main() {
-
-	// Connect to Database
-	connectToDatabase()
-
-	// Create a Table
-
-	user := User{}
-	user.Age = 21
-	user.FirstName = "Hasher"
-	user.LastName = "Gopher"
-	user.Email = "hasher.gopher@hashedin.com"
-	addUser(user)
-
-	// Query data from database
-	// users := []User{}
-	users := getUsers()
-
-	//Query for a specific user in database
-	getUser(users[0].ID)
-
-	// Cleanup the table
-	deleteAllUsers()
-
-	// load data from the json file
-	loadAllUsers("data/users.json")
-
-	// Cleanup the table
-	deleteAllUsers()
-
+	r := mux.NewRouter()
+	r.HandleFunc("/users", GetUsers)
+	r.HandleFunc("/users/{id}", GetUser)
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
